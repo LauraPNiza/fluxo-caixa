7cacb921a6c7105ac66cc3978095b8b42302a286
Laura Niza <92166534+LauraPNiza@users.noreply.github.com>
2023-06-29 22:48:29 -0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
-import styles from '../styles/home.module.css'
+import styles from '../styles/Home.module.css'
 import Image from 'next/image'
 import ovo from '../../public/ovoReduzido.png'
 
